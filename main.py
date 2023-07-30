27e449afb34b0fa99ca76cee758d7cf676761d52
HarishankarVj12 <harishankar@ix.cumberlandlabs.io>
2023-07-30 19:34:55 +0530

    Added an email enabled

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
+from sentMail import sentMail
 
 request = requests.get("https://newsapi.org/v2/everything?q=tesla&from=2023-06-30&sortBy=publishedAt&apiKey=c2f58db2e47f4cd0a95f4c863c4ba298")
 content = request.json()
-print(content)
 
+body = ""
 for article in content["articles"]:
-    print(article["author"])
-    print(article["title"])
+    body = article["author"]
+
+print(body)
+sentmail(message=body)
